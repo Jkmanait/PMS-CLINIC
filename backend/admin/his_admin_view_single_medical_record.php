855602afb5ca98a740d82c836b0e13f90c0d0a3a
jkmanait <jkmanait@gmail.com>
2024-10-02 21:12:45 +0800

    replace medical record

--- backend/admin/his_admin_view_single_medical_record.php
+++ backend/admin/his_admin_view_single_medical_record.php
@@ -99,134 +99,35 @@
                                 <div class="col-12">
                                     <div class="card-box">
                                         <div class="row">
-                                            <div class="col-xl-5">
-
-                                                <div class="tab-content pt-0">
-
-                                                    <div class="tab-pane active show" id="product-1-item">
-                                                        <img src="assets/images/medical_record.png" alt="" class="img-fluid mx-auto d-block rounded">
-                                                    </div>
-                            
-                                                </div>
-                                            </div> <!-- end col -->
+                                            <!-- Column for patient details -->
                                             <div class="col-xl-7">
                                                 <div class="pl-xl-3 mt-3 mt-xl-0">
-                                                    <h2 class="mb-3">Name : <?php echo $row->mdr_pat_name;?></h2>
+                                                    <h2 class="mb-3">Name: <?php echo $row->mdr_pat_name; ?></h2>
                                                     <hr>
-                                                    <h3 class="text-danger">Age : <?php echo $row->mdr_pat_age;?> Years</h3>
+                                                    <h3 class="text-danger">Age: <?php echo $row->mdr_pat_age; ?> Years</h3>
                                                     <hr>
-                                                    <h3 class="text-danger ">Patient Number : <?php echo $row->mdr_pat_number;?></h3>
+                                                    <h3 class="text-danger">Patient Number: <?php echo $row->mdr_pat_number; ?></h3>
                                                     <hr>
-                                                    <h3 class="text-danger ">Patient Ailment : <?php echo $row->mdr_pat_ailment;?></h3>
+                                                    <h3 class="text-danger">Patient Ailment: <?php echo $row->mdr_pat_ailment; ?></h3>
                                                     <hr>
-                                                    <h3 class="text-danger ">Date Recorded : <?php echo date("d/m/Y - h:m:s", strtotime($mysqlDateTime));?></h3>
+                                                    <h3 class="text-danger">Date Recorded: <?php echo date("d/m/Y - h:m:s", strtotime($mysqlDateTime)); ?></h3>
                                                     <hr>
+                                                </div>
+                                            </div> <!-- end col -->
+
+                                            <!-- Column for prescription details -->
+                                            <div class="col-xl-5">
+                                                <div class="pl-xl-3 mt-3 mt-xl-0">
                                                     <h2 class="align-centre">Prescription</h2>
                                                     <hr>
                                                     <p class="text-muted mb-4">
-                                                        <?php echo $row->mdr_pat_prescr;?>
+                                                        <?php echo $row->mdr_pat_prescr; ?>
                                                     </p>
                                                     <hr>
-                                                   <!--
-                                                    <form class="form-inline mb-4">
-                                                        <label class="my-1 mr-2" for="quantityinput">Quantity</label>
-                                                        <select class="custom-select my-1 mr-sm-3" id="quantityinput">
-                                                            <option value="1">1</option>
-                                                            <option value="2">2</option>
-                                                            <option value="3">3</option>
-                                                            <option value="4">4</option>
-                                                            <option value="5">5</option>
-                                                            <option value="6">6</option>
-                                                            <option value="7">7</option>
-                                                        </select>
-
-                                                        <label class="my-1 mr-2" for="sizeinput">Size</label>
-                                                        <select class="custom-select my-1 mr-sm-3" id="sizeinput">
-                                                            <option selected>Small</option>
-                                                            <option value="1">Medium</option>
-                                                            <option value="2">Large</option>
-                                                            <option value="3">X-large</option>
-                                                        </select>
-                                                    </form>
-
-                                                    <div>
-                                                        <button type="button" class="btn btn-danger mr-2"><i class="mdi mdi-heart-outline"></i></button>
-                                                        <button type="button" class="btn btn-success waves-effect waves-light">
-                                                            <span class="btn-label"><i class="mdi mdi-cart"></i></span>Add to cart
-                                                        </button>
-                                                    </div> -->
                                                 </div>
                                             </div> <!-- end col -->
-                                        </div>
-                                        <!-- end row -->
+                                        </div> <!-- end row -->
 
-                                        <!--
-                                        <div class="table-responsive mt-4">
-                                            <table class="table table-bordered table-centered mb-0">
-                                                <thead class="thead-light">
-                                                    <tr>
-                                                        <th>Outlets</th>
-                                                        <th>Price</th>
-                                                        <th>Stock</th>
-                                                        <th>Revenue</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <td>ASOS Ridley Outlet - NYC</td>
-                                                        <td>$139.58</td>
-                                                        <td>
-                                                            <div class="progress-w-percent mb-0">
-                                                                <span class="progress-value">478 </span>
-                                                                <div class="progress progress-sm">
-                                                                    <div class="progress-bar bg-success" role="progressbar" style="width: 56%;" aria-valuenow="56" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                </div>
-                                                            </div>
-                                                        </td>
-                                                        <td>$1,89,547</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Marco Outlet - SRT</td>
-                                                        <td>$149.99</td>
-                                                        <td>
-                                                            <div class="progress-w-percent mb-0">
-                                                                <span class="progress-value">73 </span>
-                                                                <div class="progress progress-sm">
-                                                                    <div class="progress-bar bg-danger" role="progressbar" style="width: 16%;" aria-valuenow="16" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                </div>
-                                                            </div>
-                                                        </td>
-                                                        <td>$87,245</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Chairtest Outlet - HY</td>
-                                                        <td>$135.87</td>
-                                                        <td>
-                                                            <div class="progress-w-percent mb-0">
-                                                                <span class="progress-value">781 </span>
-                                                                <div class="progress progress-sm">
-                                                                    <div class="progress-bar bg-success" role="progressbar" style="width: 72%;" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                </div>
-                                                            </div>
-                                                        </td>
-                                                        <td>$5,87,478</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Nworld Group - India</td>
-                                                        <td>$159.89</td>
-                                                        <td>
-                                                            <div class="progress-w-percent mb-0">
-                                                                <span class="progress-value">815 </span>
-                                                                <div class="progress progress-sm">
-                                                                    <div class="progress-bar bg-success" role="progressbar" style="width: 89%;" aria-valuenow="89" aria-valuemin="0" aria-valuemax="100"></div>
-                                                                </div>
-                                                            </div>
-                                                        </td>
-                                                        <td>$55,781</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div> -->
 
                                     </div> <!-- end card-->
                                 </div> <!-- end col-->
