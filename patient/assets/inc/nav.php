a7fe19d2225fefe1f741f7dd8d278b72f84184ca
jkmanait <jkmanait@gmail.com>
2024-10-12 10:44:20 +0800

    changes on logo and clinic name

--- patient/assets/inc/nav.php
+++ patient/assets/inc/nav.php
@@ -97,17 +97,20 @@
     </ul>
 
     <!-- LOGO -->
+    <!-- LOGO -->
     <div class="logo-box" style="display: flex; align-items: center; white-space: nowrap;">
-    <a href="index.php" class="logo text-center" style="display: flex; align-items: center;">
+        <a href="his_admin_dashboard.php" class="logo text-center" style="display: flex; align-items: center;">
         <span class="logo-lg">
-            <img src="assets/images/logo.png.png" style="height: 100px; width: 100px; margin-top: -7px;" alt="Logo">
-        </span>
-        <span class="logo-sm">
-            <img src="assets/images/logo.png.png" style="height: 100px; width: 100px; margin-top: -7px;" alt="Logo">
-        </span>
+    <img src="assets/images/logo.jpg" style="height: 100px; width: 100px; margin-top: -3px; border-radius: 45%;" alt="Logo">
+</span>
+
+            <span class="logo-sm">
+                <img src="assets/images/logo.png.png" style="height: 100px; width: 100px; margin-top: -7px;" alt="Logo">
+            </span>
+        
         <div>
-            <h1 style="font-size: 20px; color: white; margin-left: 15px; white-space: nowrap;">Mission-Buenaventure Care Family Hospital</h1>
-            <h1 style="font-size: 15px; color: white; margin-left: 15px; white-space: nowrap;">Damilag, Manolo Fortich, Bukidnon</h1>
+            <h1 style="font-size: 20px; color: white; margin-left: 15px; white-space: nowrap;">Pedia Clinic Alae</h1>
+            <h1 style="font-size: 15px; color: white; margin-left: 15px; white-space: nowrap;">Alae, Manolo Fortich, Bukidnon</h1>
         </div>
     </a>
 </div>
